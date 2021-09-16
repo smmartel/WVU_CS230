7eb54a95c912178f5abb49e62f729f4f45f4575d
smmartel <smm0059@mix.wvu.edu>
2021-09-15 22:46:13 -0400

    Addition of data binding

--- src/app/accrodian/accrodian.component.ts
+++ src/app/accrodian/accrodian.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accrodian.component.css']
 })
 export class AccrodianComponent implements OnInit {
-
+  email_title:string = "Click Here for BIG MONEY";
+  email_title2:string = "Not a Scam click here!";
   constructor() { }
 
   ngOnInit(): void {
