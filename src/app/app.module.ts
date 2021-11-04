f462b7c61985dd2c9cbb221e8cdef41f8cf9964e
smmartel <smm0059@mix.wvu.edu>
2021-11-04 07:52:47 -0400

    connection to firebase backEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,9 @@ import { EmailLayout } from './accrodian/email-layout.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UserInfoComponent } from './header/user-info.component';
 import { FormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -36,7 +39,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase())
 
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'app-603c9',
+    appId: '1:819000631350:web:86eb6bba2cb68345d53897',
+    databaseURL: 'https://app-603c9-default-rtdb.firebaseio.com',
+    storageBucket: 'app-603c9.appspot.com',
+    apiKey: 'AIzaSyBneYoASZ-lhKqAlKr51aA60BJbjadz1NY',
+    authDomain: 'app-603c9.firebaseapp.com',
+    messagingSenderId: '819000631350',
+    measurementId: 'G-9P112Q16LD',
+  },
   production: false
 };
 
